941734b35f671fbfa94d8705d88bf76bd3866d35
Kiryl Zavarotny <kirillzavorotni@gmail.com>
2019-02-12 20:46:30 +0300

    feat: add table in activitiew tab

---
+++ sources/models/userActivity.js
@@ -0,0 +1,4 @@
+export const userActivity = new webix.DataCollection({
+	url: "http://localhost:8096/api/v1/activities/",
+	save: "rest->http://localhost:8096/api/v1/activities/",
+});
--- sources/views/data.js
+++ sources/views/data.js
@@ -1,7 +1,35 @@
 import { JetView } from "webix-jet";
+import { userContacts } from "models/userContacts";
+// import { userStatuses } from "models/userStatuses";
+import { userActivity } from "models/userActivity";
+// import { userActivityType } from "models/userActivityType";
 
-export default class DataView extends JetView {
+export default class ActiveView extends JetView {
 	config() {
-		return { template: "some element" };
+
+		return {
+			rows: [
+				{ template: "first stroke", height: 50 },
+				{
+					view: "datatable",
+					localId: "active-table",
+					columns: [
+						{ id: "State", header: "", template: "{common.checkbox()}", checkValue: "Open", uncheckValue: "Close", width: 36 },
+						{ header: "Active type", width: 100 },
+						{ id: "DueDate", header: "Due date", width: 120 },
+						{ id: "Details", header: "Details", width: 300, fillspace: true },
+						{ id: "ContactID", header: ["Contact", { content: "selectFilter" }], width: 200, collection: userContacts },
+						{ template: "<span class='webix_icon wxi-pencil'></span>", width: 50 },
+						{ template: "<span class='webix_icon wxi-trash'></span>", width: 50 },
+					],
+				},
+			]
+		};
+	}
+
+	init() {
+		userActivity.waitData.then(() => {
+			this.$$("active-table").sync(userActivity);
+		});
 	}
 }
